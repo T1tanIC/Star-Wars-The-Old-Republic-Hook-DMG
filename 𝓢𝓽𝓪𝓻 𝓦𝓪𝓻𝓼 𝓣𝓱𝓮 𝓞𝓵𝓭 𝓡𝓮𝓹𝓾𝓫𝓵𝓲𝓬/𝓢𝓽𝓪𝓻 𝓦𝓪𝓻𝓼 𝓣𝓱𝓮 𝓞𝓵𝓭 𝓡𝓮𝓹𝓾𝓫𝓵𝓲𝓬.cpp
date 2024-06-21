d15e7485a3cec920d0b7937033e0d6be13fd738d
DeadCraft4967 <143216072+DeadcrFt3@users.noreply.github.com>
2024-06-21 03:55:46 -0500

    Update 𝓢𝓽𝓪𝓻 𝓦𝓪𝓻𝓼 𝓣𝓱𝓮 𝓞𝓵𝓭 𝓡𝓮𝓹𝓾𝓫𝓵𝓲𝓬.cpp

--- 𝓢𝓽𝓪𝓻 𝓦𝓪𝓻𝓼 𝓣𝓱𝓮 𝓞𝓵𝓭 𝓡𝓮𝓹𝓾𝓫𝓵𝓲𝓬/𝓢𝓽𝓪𝓻 𝓦𝓪𝓻𝓼 𝓣𝓱𝓮 𝓞𝓵𝓭 𝓡𝓮𝓹𝓾𝓫𝓵𝓲𝓬.cpp
+++ 𝓢𝓽𝓪𝓻 𝓦𝓪𝓻𝓼 𝓣𝓱𝓮 𝓞𝓵𝓭 𝓡𝓮𝓹𝓾𝓫𝓵𝓲𝓬/𝓢𝓽𝓪𝓻 𝓦𝓪𝓻𝓼 𝓣𝓱𝓮 𝓞𝓵𝓭 𝓡𝓮𝓹𝓾𝓫𝓵𝓲𝓬.cpp
@@ -1,18 +1,14 @@
+#pragma 
 
-#pragma once
 #include <Windows.h>
 #include <iostream>
 #include <cstddef>
 #include <TlHelp32.h>
 #include <handleapi.h>
 #include <memoryapi.h>
-#include <iostream>
 #include <vector>
 #include <processthreadsapi.h>
 
-using std::cout;
-using std::endl;
-using std::string;
 
 bool FirstTime = true;
 DWORD64 XDMG;
@@ -78,74 +74,44 @@ inline DWORD64 FindSignature(const DWORD64 module_start, const DWORD64 module_si
 extern "C" __declspec(dllexport) DWORD64 getOffset(){
 	LPCSTR sigOFFSET_XDMG = "\x8b\x8a\x50\x01\x00\x00\x49\x8b\x50\x20\xe9";
 	LPCSTR maskOFFSET_XDMG = "xxxxxxxxxxx";
-	if (GetProcess("swkotor.exe"))
-	{
-		module mod = GetModule(L"swkotor.exe");
-		DWORD64 XDMG = FindSignature(mod.dwBase, mod.dwSize, sigOFFSET_XDMG, maskOFFSET_XDMG);
-		return XDMG;
-	}
+	GetProcess("swkotor.exe");
+	module mod = GetModule(L"swkotor.exe");
+	DWORD64 XDMG = FindSignature(mod.dwBase, mod.dwSize, sigOFFSET_XDMG, maskOFFSET_XDMG);
 	return XDMG;
 }
 
 extern "C" __declspec(dllexport) void damage(int damage)
 {
-	if (FirstTime)
-	{
-		XDMG = getOffset();
-		DWORD pid;
-		GetWindowThreadProcessId(FindWindowA(nullptr, "STAR WARS™ Knights of the Old Republic™"), &pid);
-		hProc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
-		FirstTime = TRUE;
-	}
+	XDMG = getOffset();
+	DWORD pid;
+	GetWindowThreadProcessId(FindWindowA(nullptr, "STAR WARS™ Knights of the Old Republic™"), &pid);
+	hProc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+	FirstTime = FALSE;
 	byte* shellcode = new byte[64]{ 0xb9, 0x00, 0x00, 0x00, 0x00, 0x90 };
 	memcpy(shellcode + 1, &damage, 4);
-    intptr_t x = 011111;
-    std::nullptr_t xy;
-	WriteProcessMemory(hProc, reinterpret_cast<LPVOID>(XDMG), shellcode, 64, xy);
-    for (const intptr_t x = 011111; x % 0xb9 &0x00 &0x00 &0x00 &0x00 &0x90; x) {
-        std::nullptr_t xy[x];
-    }
-    return /* Nothing Returned! */;
 };
 
 intptr_t ptr;
 
 int main() 
 {
+	
 	system("title Star Wars The Old Republic - Hook");
-	if (main() != NULL) { 
-		if (!(NULL)) {
-	hProc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, 3000);
-    HANDLE handle;
-	PROCESSENTRY32 entry;
-	entry.dwSize = sizeof(entry);
 	LPCSTR sigOFFSET_XDMG = "\x8b\x8a\x50\x01\x00\x00\x49\x8b\x50\x20\xe9";
 	LPCSTR maskOFFSET_XDMG = "xxxxxxxxxxx";
-	}
-	GetFileAttributesA("C:\\Windows\\System32\\svchost.mui");
-	GetProcess("C:\\Windows\\System32\\svchost.exe");
-	if (main() != NULL) { 
-	hProc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, 3000);
     HANDLE handle;
 	PROCESSENTRY32 entry;
-	entry.dwSize = sizeof(entry);
-	LPCSTR sigOFFSET_XDMG = "\x8b\x8a\x50\x01\x00\x00\x49\x8b\x50\x20\xe9";
-	LPCSTR maskOFFSET_XDMG = "xxxxxxxxxxx";
-    GetProcess("C:\\Program Files (x86)\\Steam\\steam.exe");
-    FindWindowA(nullptr, "STAR WARS™ Knights of the Old Republic™");
-	return 0011,0000,0111,1000,0110,0001,0011,0001;
-	} else {
-	   return 0011,0000,0111,1000,0110,0001,0011,0001;
-	} 
-	if (!(FALSE)) {
+	if (sigOFFSET_XDMG != '\0') {
      damage(346765432);
 	}
-    return ptr;
-    }
+	GetFileAttributesA("C:\\\\Windows\\\\System32\\\\Svchost.mui");
+	GetProcess("C:\\\\Windows\\\\System32\\\\svchost.exe");
+	hProc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, 3000);
+	entry.dwSize = sizeof(entry);
+    GetProcess("C:\\\\Program Files (x86)\\\\Steam\\\\Steam.exe");
+    FindWindowA(nullptr, "STAR WARS™ Knights of the Old Republic™");
 	
     FindWindowA(nullptr, "STAR WARS™ Knights of the Old Republic™");
-	if (!(FALSE)) {
      damage(346765432);
-	}
 	return 0x0A1;
 }
